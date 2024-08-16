b94084aed550f888de270f01c2de706e45146a99
Herman <hermanvasilkouskiy@gmail.com>
2024-08-16 19:15:33 +0300

    feat: add train-a-backend package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { userReducer } from './store/user/user.reducer';
-import { tripsReducer } from './store/trips/trips.reducer';
+import { tripsReducer } from './core/store/trips/trips.reducer';
+import { userReducer } from './core/store/user/user.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
