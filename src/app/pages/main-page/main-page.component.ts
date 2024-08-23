e0ae4a02d2ccb628b2b7bcbe8851e3223c2e4663
Natalia Repkina <pn.88@mail.ru>
2024-08-23 16:18:20 +0300

    fix: remove test data from main

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -7,20 +7,4 @@ import { CarriageSelectedSeats } from '../../shared/components/carriage/carriage
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-  carriage: Carriage = {
-    code: '123',
-    name: 'mmm',
-    rows: 16,
-    leftSeats: 2,
-    rightSeats: 3,
-  };
-
-  carriageNumber: number = 1;
-
-  occupiedSeat: number[] = [1, 3, 4, 5, 6, 7];
-
-  onSelectedSeatsChange(seatsData: CarriageSelectedSeats) {
-    console.log('Selected seats:', seatsData.selectedSeats);
-  }
-}
+export class MainPageComponent {}
