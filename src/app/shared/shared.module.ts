029dec4f8d5bf4d9697db16e362508c356cab0cf
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2024-08-23 16:39:39 +0300

    Feat/train visualization (#5)
    * fix: burger icon position
    
    * feat: add basic logic for carriage component
    
    * fix: change carriage component logic
    
    * feat: add splitSeats pipe
    
    * fix: change splitSeats pipe
    
    * feat: add basic template for carriage
    
    * feat: add styles for carriage
    
    * fix: fix style
    
    * refactor: add description and remove test from main
    
    * refactor: add description and remove test from main
    
    * fix: add required flag for every necessary input
    
    * refactor: format
    
    * fix: remove unused parameters from carriage
    
    * feat: add trackBy in carriage component
    
    * fix: remove test data from main
    
    * refactor: format

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarriageComponent } from './components/carriage/carriage.component';
+import { SplitSeatsPipe } from './pipes/split-seats.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [CarriageComponent, SplitSeatsPipe],
   imports: [CommonModule],
+  exports: [CarriageComponent],
 })
 export class SharedModule {}
