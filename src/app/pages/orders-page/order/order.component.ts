0219dd25b2090cb347713120dd9ba4e5e2b37f53
Natalia Repkina <pn.88@mail.ru>
2024-08-25 15:54:50 +0300

    feat: add order visualization component

--- src/app/pages/orders-page/order/order.component.ts
+++ src/app/pages/orders-page/order/order.component.ts
@@ -1,11 +1,50 @@
-import { Component, Input } from '@angular/core';
-import { Order } from '../../../features/trips/models/order.model';
+import { Component, OnInit } from '@angular/core';
+// import { Order } from '../../../features/trips/models/order.model';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss',
 })
-export class OrderComponent {
-  @Input() order!: Order;
+export class OrderComponent implements OnInit {
+  // @Input() order!: Order;
+
+  /*
+ - Станция начала поездки
+- Время начала поездки - "MMMM dd hh:mm".
+- Станция конечной поездки
+- Время конечной поездки - "MMMM dd hh:mm".
+- Продолжительность поездки - отображается в часах и минутах
+- Тип вагона
+- Номер вагона
+- Номер места
+- Цена - $50.00
+*/
+  startStation: string = 'Station Start';
+
+  startTime: string = 'December 25 03:45';
+
+  endStation: string = 'Station End';
+
+  endTime: string = 'December 25 06:12';
+
+  durationTrip: string = '02:37';
+
+  typeCarriage: string = 'standard';
+
+  numberCarriage: number = 1;
+
+  numberSeat: number = 10;
+
+  price: string = '50.00';
+
+  status: string = 'active';
+
+  ngOnInit(): void {
+    console.log();
+  }
+
+  delete() {
+    console.log('delete');
+  }
 }
