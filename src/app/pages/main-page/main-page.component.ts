017918871d121762a8f518dfb17bc2858f9fc011
Natalia Repkina <pn.88@mail.ru>
2024-08-27 12:53:51 +0300

    refactor: clean main page

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-
-import * as actions from '../../core/store/user/user.actions';
-import { selectUser } from '../../core/store/user/user.selectors';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent implements OnInit {
-  constructor(private store: Store) {}
-
-  ngOnInit() {
-    // eslint-disable-next-line @ngrx/avoid-dispatching-multiple-actions-sequentially
-    this.store.dispatch(actions.signUp({ email: 'test@mail.com', password: '12345aA!!' }));
-
-    // eslint-disable-next-line @ngrx/avoid-dispatching-multiple-actions-sequentially
-    this.store.dispatch(actions.signIn({ email: 'test@mail.com', password: '12345aA!!' }));
-
-    console.log(`User ${this.store.select(selectUser)}`);
-  }
-}
+export class MainPageComponent {}
