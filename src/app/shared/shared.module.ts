3896e2e740ac68a049042465644121fbacfe1d56
Natalia Repkina <pn.88@mail.ru>
2024-08-29 11:31:01 +0300

    fix: remove unused pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarriageComponent } from './components/carriage/carriage.component';
 import { SplitSeatsPipe } from './pipes/split-seats.pipe';
-import { TransformOrderPipe } from './pipes/order-view.pipe';
 
 @NgModule({
-  declarations: [CarriageComponent, SplitSeatsPipe, TransformOrderPipe],
+  declarations: [CarriageComponent, SplitSeatsPipe],
   imports: [CommonModule],
-  exports: [CarriageComponent, TransformOrderPipe],
+  exports: [CarriageComponent],
 })
 export class SharedModule {}
