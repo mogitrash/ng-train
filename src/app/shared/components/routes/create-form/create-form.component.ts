0fd4eb979c132c6dd368fe9bf5c642756fb19368
Aleksa013 <velma86@bk.ru>
2024-09-01 15:09:13 +0300

    feat: create new route is ready

--- src/app/shared/components/routes/create-form/create-form.component.ts
+++ src/app/shared/components/routes/create-form/create-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormArray, FormBuilder, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 import { Station } from '../../../../features/trips/models/station.model';
 import { Carriage } from '../../../../features/trips/models/carriage.model';
 import { selectCarriages, selectStations } from '../../../../core/store/trips/trips.selectors';
+import { createRoute } from '../../../../core/store/trips/trips.actions';
+import { selectReasonError } from '../../../../core/store/user/user.selectors';
 
 @Component({
   selector: 'app-create-form',
@@ -13,13 +20,21 @@ import { selectCarriages, selectStations } from '../../../../core/store/trips/tr
 })
 export class CreateFormComponent {
   protected createForm = this.FB.nonNullable.group({
-    stations: this.FB.array([new FormControl('')]),
-    carriages: this.FB.array([new FormControl('')]),
+    stations: this.FB.nonNullable.array([new FormControl('')]),
+    carriages: this.FB.nonNullable.array([new FormControl('')]),
   });
 
   public stations$: Observable<Station[]>;
 
   public carriages$: Observable<Carriage[]>;
+
+  private hasError$: Observable<string>;
+
+  private snackBar = inject(MatSnackBar);
+
+  private horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+
+  private verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   constructor(
     private readonly store: Store,
@@ -27,14 +42,15 @@ export class CreateFormComponent {
   ) {
     this.stations$ = this.store.select(selectStations);
     this.carriages$ = this.store.select(selectCarriages);
+    this.hasError$ = this.store.select(selectReasonError);
   }
 
   public get carriages(): FormArray {
-    return this.createForm.get('stations') as FormArray;
+    return this.createForm.get('carriages') as FormArray;
   }
 
   public get stations(): FormArray {
-    return this.createForm.get('carriages') as FormArray;
+    return this.createForm.get('stations') as FormArray;
   }
 
   public addItem(goal: 'station' | 'carriage'): void {
@@ -44,5 +60,33 @@ export class CreateFormComponent {
 
   public onSubmit() {
     console.log(this.createForm.value);
+    if (
+      this.createForm.value.carriages!.every((carriage) => {
+        return typeof carriage === 'string';
+      })
+    ) {
+      const pathes: number[] = [];
+      this.createForm.value.stations!.forEach((path: string | null) => {
+        pathes.push(Number(path));
+      });
+
+      console.log(pathes, this.createForm.value.carriages!);
+
+      this.store.dispatch(
+        createRoute({
+          path: pathes,
+          carriages: this.createForm.value.carriages!.slice(0, -1),
+        }),
+      );
+    }
+    this.createForm.reset();
+    this.openSnackBarError('New route was create');
+  }
+
+  private openSnackBarError(message: string) {
+    this.snackBar.open(`${message}`, 'Close', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
   }
 }
