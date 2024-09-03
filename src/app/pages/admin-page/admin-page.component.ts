0ee7594d1a67a55de1cd9d67bc3c3cea0016898c
Aleksa013 <velma86@bk.ru>
2024-09-03 17:23:42 +0300

    fix: fix first behavior link

--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit, OnDestroy {
   public activeButtons: 'stations' | 'carriages' | 'routes' | '' = '';
+
+  private path!: string[];
+
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {
+    this.path = this.route.url.split('/');
+    this.activeButtons = this.path[2] ? (this.path[2] as 'stations' | 'carriages' | 'routes') : '';
+    console.log(this.activeButtons);
+  }
 
   public makeActive(activeButtons: 'stations' | 'carriages' | 'routes') {
     this.activeButtons = activeButtons;
+    console.log(this.path);
+  }
+
+  ngOnDestroy() {
+    this.activeButtons = '';
   }
 }
